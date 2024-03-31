6ca1abc591b6dc34c5a2c58031802201210f2884
Taha Alfoghi <102795632+tahaalfoghi@users.noreply.github.com>
2024-03-31 18:32:26 +0200

    rename folders

--- obj/Debug/net8.0/northwindAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/northwindAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("northwindAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+429c87ff098d514cf5f12e371f0d2d93108cb4bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+132e4dc6e0fde5bd189c9a4f7b70dbc009bdd933")]
 [assembly: System.Reflection.AssemblyProductAttribute("northwindAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("northwindAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
